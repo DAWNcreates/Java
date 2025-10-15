e378971dcd7bff677d133a56d035de387631fa3a
sahar jleli <saharproex@gmail.com>
2025-10-15 10:20:39 -1200

    sahars first part fixed missing classes

--- java/Gestionnaire/GestionnaireFinances.java
+++ java/Gestionnaire/GestionnaireFinances.java
@@ -3,10 +3,43 @@ package Gestionnaire;
 
 
 public class GestionnaireFinances {
-    private double totalRevenus;            // Somme totale des revenus
-    private double totalDepenses;           // Somme totale des dépenses
-    //private Map<String, Double> paiements; // Clé = ID paiement, Valeur = montant
-    //private Map<String, String> factures;  // Clé = ID facture, Valeur = statut
-    //private class fichier  rapport;        // Indique si un rapport a été généré
+
+
+    private double totalRevenus;
+    private double totalDepenses;
+    //private Map<String, Double> paiements = new HashMap<>();
+    //private Map<String, String> factures = new HashMap<>();
+    private boolean rapportsGeneres;
+
+    public GestionnaireFinances() {
+    }
+
+    public void ajouterPaiement(String idPaiement, double montant) {
+    }
+
+    public void supprimerPaiement(String idPaiement) {
+    }
+
+    public void ajouterFacture(String idFacture, String statut) {
+    }
+
+    public void supprimerFacture(String idFacture) {
+    }
+
+    public void genererRapport() {
+    }
+
+    public void afficherResume() {
+    }
+
+    public double getTotalRevenus() { return totalRevenus; }
+    public void setTotalRevenus(double totalRevenus) { this.totalRevenus = totalRevenus; }
+
+    public double getTotalDepenses() { return totalDepenses; }
+    public void setTotalDepenses(double totalDepenses) { this.totalDepenses = totalDepenses; }
+
+    public boolean isRapportsGeneres() { return rapportsGeneres; }
+    public void setRapportsGeneres(boolean rapportsGeneres) { this.rapportsGeneres = rapportsGeneres; }
 
 }
+
