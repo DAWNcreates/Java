c8db5ddfb6a11eb53278455084226bba2257dcef
Marwa <marwarok09@gmail.com>
2025-10-23 22:56:39 +0100

    Ajout des exceptions et methodes de validations

---
+++ src/application/Incrementable.java
@@ -0,0 +1,10 @@
+
+package application;
+
+
+
+
+public interface Incrementable {
+    int incrémenter (int val ,int inc);
+
+}
---
+++ src/application/Modifiable.java
@@ -0,0 +1,8 @@
+
+package application;
+
+import java.util.Scanner;
+public interface Modifiable {
+int incrémenter (int val ,int inc);
+int décrémenter (int val,int inc);
+int raz (int val); }
